cb577af15f7212f248a5348e3c4a10d4d9319331
Samuel Micka <micka@samuels-air.home>
2016-10-01 22:14:27 -0600

    Forgot to add these two, I changed the timeouts so that they would work

--- Client.py
+++ Client.py
@@ -11,22 +11,22 @@ if __name__ == '__main__':
     msg_L = ['The use of COBOL cripples the mind; its teaching should, therefore, be regarded as a criminal offense. -- Edsgar Dijkstra',
             'C makes it easy to shoot yourself in the foot; C++ makes it harder, but when you do, it blows away your whole leg. -- Bjarne Stroustrup',
             'A mathematician is a device for turning coffee into theorems. -- Paul Erdos',
-            'Grove giveth and Gates taketh away. -- Bob Metcalfe (inventor of Ethernet) on the trend of hardware speedups not being able to keep up with software demands',
-            'Wise men make proverbs, but fools repeat them. -- Samuel Palmer (1805-80)']
+            'Grove giveth and Gates taketh away. -- Bob Metcalfe (inventor of Ethernet) on the trend of hardware speedups not being able to keep up with software demands']#,
+#           'Wise men make proverbs, but fools repeat them. -- Samuel Palmer (1805-80)']
     
      
-    timeout = 2 #send the next message if not response
+    timeout = 100 #send the next message if not response
     time_of_last_data = time.time()
      
     rdt = RDT.RDT('client', args.server, args.port)
     for msg_S in msg_L:
         print('Converting: '+msg_S)
-        rdt.rdt_1_0_send(msg_S)
+        rdt.rdt_2_1_send(msg_S)
        
         # try to receive message before timeout 
         msg_S = None
         while msg_S == None:
-            msg_S = rdt.rdt_1_0_receive()
+            msg_S = rdt.rdt_2_1_receive()
             if msg_S is None:
                 if time_of_last_data + timeout < time.time():
                     break
